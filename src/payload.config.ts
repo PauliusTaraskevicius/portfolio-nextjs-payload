3897066fd3a05296d144841bb1dd6979fc6a2a29
PauliusTaraskevicius <fixas1029@gmail.com>
2025-01-17 16:05:09 +0200

    tailwindcss, shadcn, s3 storage setup

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,8 @@ import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
+import { s3Storage } from '@payloadcms/storage-s3'
+
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 
@@ -33,5 +35,19 @@ export default buildConfig({
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.S3_BUCKET || '',
+      config: {
+        region: process.env.S3_REGION || '',
+        endpoint: process.env.S3_ENDPOINT || '',
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+        },
+      },
+    }),
   ],
 })
